3c1e15fedce31b8e24cdeac6d200cdaf1a52ea36
Abdelrahman Ahmed <73317105+abdelrahman252@users.noreply.github.com>
2023-03-27 14:23:42 +0200

    fixing some bugs

--- back-end/resources/views/livewire/product-table.blade.php
+++ back-end/resources/views/livewire/product-table.blade.php
@@ -15,83 +15,83 @@
     @include('layouts.notification')
     <div class="products-section-row">
         @foreach($products as $product)
-            @if($product->inventory->quantity <1 || $product->status!=1)
-                <div class="product">
-                    <div class="productImg">
-                        <img src="imgs/{{$product->pro_image1}}" alt="" />
-                        <img src="imgs/{{$product->pro_image2}}" alt="" />
-
-                    </div>
-
-                    <div class="productInfo">
-                        <p>{{$product->brand->name}}</p>
-                        <p><strong> {{$product->productName}} </strong></p>
-                        <span class="price"> <del>{{$product->productCost}}$ </del> {{$product->price}}$</span>
-
-                        <p style="color:black"><strong>unvailable </strong></p>
-
-                    </div>
-
-                    <form  wire:submit.prevent="addItemToCart({{$product->id}})">
-                        @csrf
-
-                        <button type="button" disabled style="background: gray"  type="submit" class="add-cart addToCart" >Add to cart</button>
-                    </form>
-
-                    <form wire:submit.prevent=" addItemToWishlist({{$product->id}})">
-                        @csrf
-                        <button class="wishList addToWishList" >
-                            <div>
-                                <i class="fa-regular fa-heart " onclick="myFunction(this)"></i>
-                            </div>
-
-                        </button>
-
-                    </form>
+        @if($product->inventory->quantity <1 || $product->status!=1)
+            <div class="product">
+                <div class="productImg">
+                    <img src="imgs/{{$product->pro_image1}}" alt="" />
+                    <img src="imgs/{{$product->pro_image2}}" alt="" />
 
                 </div>
+
+                <div class="productInfo">
+                    <p>{{$product->brand->name}}</p>
+                    <p><strong> {{$product->productName}} </strong></p>
+                    <span class="price"> <del>{{$product->productCost}}$ </del> {{$product->price}}$</span>
+
+                    <p style="color:black"><strong>unvailable </strong></p>
+
+                </div>
+
+                <form wire:submit.prevent="addItemToCart({{$product->id}})">
+                    @csrf
+
+                    <button type="button" disabled style="background: gray" type="submit" class="add-cart addToCart">Add
+                        to cart</button>
+                </form>
+
+                <form wire:submit.prevent="addItemToWishlist({{$product->id}})">
+                    @csrf
+                    <button class="wishList addToWishList">
+                        <div>
+                            <i class="fa-regular fa-heart " onclick="myFunction(this)"></i>
+                        </div>
+
+                    </button>
+
+                </form>
+
+            </div>
             @else
-                <div class="product">
-                    <div class="productImg">
-                        <img src="imgs/{{$product->pro_image1}}" alt="" />
-                        <img src="imgs/{{$product->pro_image2}}" alt="" />
-
-                    </div>
-
-                    <div class="productInfo">
-                        <p>{{$product->brand->name}}</p>
-                        <p><strong> {{$product->productName}} </strong></p>
-                        <span class="price"> <del>{{$product->productCost}}$ </del> {{$product->price}}$</span>
-
-                        <p style="color:black"><strong>in stock :{{$product->inventory->quantity}} </strong></p>
-
-                    </div>
-
-                    <form wire:submit.prevent="addItemToCart({{$product->id}})">
-                        @csrf
-                        {{--                    <input wire:model="quantity.{{$product->id}}" type="number">--}}
-                        <button type="submit" class="add-cart addToCart" >Add to cart</button>
-                    </form>
-
-                    <form wire:submit.prevent=" addItemToWishlist({{$product->id}})">
-                        @csrf
-                        <button class="wishList addToWishList" >
-                            <div>
-                                <i class="fa-regular fa-heart " onclick="myFunction(this)"></i>
-                            </div>
-
-                        </button>
-
-                    </form>
+            <div class="product">
+                <div class="productImg">
+                    <img src="imgs/{{$product->pro_image1}}" alt="" />
+                    <img src="imgs/{{$product->pro_image2}}" alt="" />
 
                 </div>
+
+                <div class="productInfo">
+                    <p>{{$product->brand->name}}</p>
+                    <p><strong> {{$product->productName}} </strong></p>
+                    <span class="price"> <del>{{$product->productCost}}$ </del> {{$product->price}}$</span>
+
+                    <p style="color:black"><strong>in stock :{{$product->inventory->quantity}} </strong></p>
+
+                </div>
+
+                <form wire:submit.prevent="addItemToCart({{$product->id}})">
+                    @csrf
+                    {{--                    <input wire:model="quantity.{{$product->id}}" type="number">--}}
+                    <button type="submit" class="add-cart addToCart">Add to cart</button>
+                </form>
+
+                <form wire:submit.prevent=" addItemToWishlist({{$product->id}})">
+                    @csrf
+                    <button class="wishList addToWishList">
+                        <div>
+                            <i class="fa-regular fa-heart " onclick="myFunction(this)"></i>
+                        </div>
+
+                    </button>
+
+                </form>
+
+            </div>
             @endif
 
 
-        @endforeach
+            @endforeach
 
 
 
     </div>
-{{--</section>--}}
-
+    {{--</section>--}}
