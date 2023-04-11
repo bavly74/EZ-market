740d3342ba98c56cccd5f94b081c212630aa0082
Abdelrahman Ahmed <73317105+abdelrahman252@users.noreply.github.com>
2023-04-11 03:10:43 +0200

    adding the details

--- back-end/resources/views/links.blade.php
+++ back-end/resources/views/links.blade.php
@@ -6,6 +6,13 @@
     crossorigin="anonymous"
     referrerpolicy="no-referrer"
 />
+<link rel="stylesheet" href="/css/productDetails.css" />
 
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
+<link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+<link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
 
